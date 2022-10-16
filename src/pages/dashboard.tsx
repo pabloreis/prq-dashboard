26cf090bb3791b7026ca04f3dfebb9832b2e96ae
Pablo Reis <pablo.reis@storyteq.com>
2022-10-16 20:56:03 +0200

    chore: add Sidebar comp to dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from '@chakra-ui/react';
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex my="6" mx="auto" px="6" maxWidth={1480} w="100%">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
